9dcf78eaad49513f49f2468e3f759edacc7985fd
Vishal Rathod <vrathod591@gmail.com>
2026-01-20 01:39:16 +0530

    working on Navbar design and Profile design+api

--- src/Component/Profile/Profile.jsx
+++ src/Component/Profile/Profile.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getProfile } from "../../feature/getProfile/getProfileSlice";
+import { useSelector } from "react-redux";
+
 const Profile=()=>{
+
+    const [profileData, setProfileData] = useState({});
+
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getProfile());
+    },[]);
+
+    const getProfileState = useSelector((store)=>store.getProfileState);
+
+    useEffect(()=>{
+        if(getProfileState?.list?.data){
+            setProfileData(getProfileState?.list?.data)
+        }
+    },[getProfileState]);
+
+
     return(
-        <div>Profile</div>
+        <div>Profile
+            <h2>{profileData.firstName}</h2>
+            <img src={profileData.photoUrl} />
+        </div>
     )
 }
 
