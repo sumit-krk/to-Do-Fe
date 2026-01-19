849c9cf6dcd6bc2fd6bb6bd6d78b3eefadcd7b17
Vishal Rathod <vrathod591@gmail.com>
2026-01-19 20:23:17 +0530

    Todo code

--- src/Component/Todo/Todo.jsx
+++ src/Component/Todo/Todo.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {React , useState} from 'react'
 
 const Todo = () => {
+
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodo(e.target.value);
+    console.log(e.target.value, "value:-");
+  }
+
   return (
-    <div style={{textAlign:"center", marginTop:"100px" }}>Todo</div>
+    <div style={{textAlign:"center", marginTop:"20px" }}>
+      <h2>To do App</h2>
+      <div style={{border:"1px solid black", width:"50%", margin:"20px auto", padding:"20px", borderRadius:"8px"}}><form>
+        <label htmlFor="todoInput">Enter your Todo :- </label>
+        <input type="text" name="todoInput" id="" placeholder='Write here' value={todo} />
+        <button onSubmit={handleSubmit} type="submit">Add</button>
+      </form>
+      </div>
+    </div>
   )
 }
 
